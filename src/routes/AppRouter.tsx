d6cf2a271b5e7032fbc6c0c25ad286e2aba7a2e9
연가은 <gani@yeonga-eun-ui-MacBookPro.local>
2026-04-29 10:39:10 +0900

    HashRouter로 변경

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { ROUTE_PATHS } from '@/constants/route_paths';
-import NotFoundPage from '@/views/common/NotFoundPage';
-import AttendanceCalculatorPage from '@/views/attendance/AttendanceCalculatorPage';
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
+import { ROUTE_PATHS } from "@/constants/route_paths";
+import NotFoundPage from "@/views/common/NotFoundPage";
+import AttendanceCalculatorPage from "@/views/attendance/AttendanceCalculatorPage";
 
 export default function AppRouter(): React.ReactNode {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path={ROUTE_PATHS.ROOT} element={<AttendanceCalculatorPage />} />
         <Route path={ROUTE_PATHS.NOT_FOUND} element={<NotFoundPage />} />
         <Route path="*" element={<Navigate to={ROUTE_PATHS.ROOT} replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
